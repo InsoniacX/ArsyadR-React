58b1781fa193179ede8f7621567ddf90cd7b9f58
InsoniacX <riandaarsyad@gmail.com>
2026-03-08 23:25:53 +0700

    fix: routing problem

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,9 +4,9 @@ import { Homepage, Project, Contact } from "@/pages";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="./" element={<Homepage />} />
-      <Route path="./project" element={<Project />} />
-      <Route path="./contact" element={<Contact />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 };
