298dac0ca2e3181efae3e81801dad17c630055bd
InsoniacX <riandaarsyad@gmail.com>
2026-03-09 01:34:56 +0700

    fix: Set the Default theme to dark mode

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = ({
   isDarkMode: boolean;
   toggleDarkMode: () => void;
 }) => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(true);
 
   const navLinks = [
     { href: "/", label: "Home" },
